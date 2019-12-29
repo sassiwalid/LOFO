d34d32f09017b5f402c9913e80dd226d9ddd436d
Sassi Walid <swa@mobilepowered.fr>
2019-12-29 09:42:12 +0100

    Stucture commit

---
+++ Pods/Target Support Files/Pods-LOFOTunisia-LOFOTunisiaUITests/Pods-LOFOTunisia-LOFOTunisiaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LOFOTunisia_LOFOTunisiaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LOFOTunisia_LOFOTunisiaUITests
+@end
---
+++ Pods/Target Support Files/Pods-LOFOTunisiaTests/Pods-LOFOTunisiaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LOFOTunisiaTests : NSObject
+@end
+@implementation PodsDummy_Pods_LOFOTunisiaTests
+@end
